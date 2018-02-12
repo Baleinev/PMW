e5e7fa97931b6f59af688015c062ad87c10ec55f
Benoit Gianinetti <benoit.gianinetti@gmail.com>
2018-02-12 19:51:49 +0100

    Add shape and line width options

--- frontend/mobile/src/Drawing/index.js
+++ frontend/mobile/src/Drawing/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import {Slider, Radio} from 'antd';
 import SketchPad from './SketchPad';
-import {TOOL_ELLIPSE} from './tools';
+import {TOOL_ELLIPSE, TOOL_LINE, TOOL_PENCIL, TOOL_RECTANGLE} from './tools';
 import IO from 'socket.io-client';
 
 const wsClient = IO(`ws://127.0.0.1:2000`);
@@ -12,7 +13,7 @@ export default class Home extends React.Component {
     super(props);
 
     this.state = {
-      tool: TOOL_ELLIPSE,
+      tool: TOOL_PENCIL,
       size: 2,
       color: '#000000',
       fill: false,
@@ -40,6 +41,18 @@ export default class Home extends React.Component {
               items={items}
               tool={tool}
               onCompleteItem={this.sendItem}
+          />
+          <Radio.Group defaultValue={TOOL_PENCIL} onChange={(e) => this.setState({tool:e.target.value})}>
+            <Radio.Button default value={TOOL_PENCIL}>pencil</Radio.Button>
+            <Radio.Button value={TOOL_LINE}>line</Radio.Button>
+            <Radio.Button value={TOOL_ELLIPSE}>ellipse</Radio.Button>
+            <Radio.Button value={TOOL_RECTANGLE}>rectangle</Radio.Button>
+          </Radio.Group>
+          <Slider
+              min={1}
+              max={10}
+              defaultValue={2}
+              onChange={(e) => this.setState({size: e})}
           />
         </div>
     )
