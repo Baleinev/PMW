c7a6cd5065875340e423ab0357ce08c10f9baa93
Benoit Gianinetti <benoit.gianinetti@gmail.com>
2018-02-12 16:55:02 +0100

    Add basic sketchpad

--- frontend/mobile/src/Drawing/index.js
+++ frontend/mobile/src/Drawing/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import SketchPad from './SketchPad';
+import {TOOL_ELLIPSE} from './tools';
+import IO from 'socket.io-client';
+
+const wsClient = IO(`ws://127.0.0.1:2000`);
 
 export default class Home extends React.Component {
 
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tool: TOOL_ELLIPSE,
+      size: 2,
+      color: '#000000',
+      fill: false,
+      fillColor: '#444444',
+      items: []
+    }
+  }
+
+  sendItem = (item) => {
+    wsClient.emit('addItem', item);
+  };
+
   render() {
+    const { tool, size, color, fill, fillColor, items } = this.state;
+
     return (
         <div>
-          A nice drawing app
+          <SketchPad
+              width={500}
+              height={500}
+              animate={true}
+              size={size}
+              color={color}
+              fillColor={fill ? fillColor : ''}
+              items={items}
+              tool={tool}
+              onCompleteItem={this.sendItem}
+          />
         </div>
     )
   }
--- frontend/mobile/src/Main.js
+++ frontend/mobile/src/Main.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Home from './Home';
 import Drawing from './Drawing';
