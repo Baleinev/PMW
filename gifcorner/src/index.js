8d01a93af0f18ec1e51d3016c1f9a5ff4032eba8
Colin Lavanchy <colin.lavanchy@heig-vd.ch>
2018-04-26 05:29:31 +0200

    Test

--- gifcorner/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import App from './Main';
-
-render(
-    <App />,
-    document.getElementById('root')
-);
--- gifcorner/src/screen/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style/index.css';
-import Display from './Display';
-
-ReactDOM.render(<Display />, document.getElementById('root'));
