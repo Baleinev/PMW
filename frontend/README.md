257687d13186a7cbc1c7c1e60fc2717aeb4815e9
Colin Lavanchy <colin.lavanchy@heig-vd.ch>
2018-04-27 01:03:38 +0200

    k

--- frontend/README.md
+++
@@ -1,3 +0,0 @@
-# PMW Mobile frontend
-
-This is the react-frontend for all mobile applications.
--- frontend/src/Sketchpad/mobile/socket.js
+++
@@ -1,7 +0,0 @@
-import IO from 'socket.io-client';
-import React from 'react';
-
-const network = CONFIG.network;
-export default function createSocket() {
-    return IO("ws://" + network.host + ":" + network.appsPort);
-}
